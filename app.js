e2d7d1f13314975a0051e07dafd3b726004bd511
samuel21695 <samuel21695@gmail.com>
2023-10-13 09:31:07 +0900

    test createServer-2

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
 const server = http.createServer( (req, res) => {
-  console.log(req);
+  console.log(req.url);
+  res.writeHead(200, {"Content-type": "text/html"});
+  let doc =`<html> <head> </head> <body> </body> <h1> test </h1> </body> </html>`
+  res.end(doc);
 })
 
 server.listen(1234);
